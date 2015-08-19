caffc8728fb6bd391eaab7d59fe94ca17066131e
Suriyaa Kudo <isc.suriyaa@gmail.com>
2015-08-19 15:06:09 +0200

    Upload src folder

---
+++ src/isc/pedia/iSCPediaActivity.java
@@ -0,0 +1,25 @@
+package isc.pedia;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+
+import org.apache.cordova.DroidGap;
+
+public class iSCPediaActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		super.loadUrl("file:///android_asset/www/index.html");
+
+		String currentUA = this.appView.getSettings().getUserAgentString();
+
+		try {
+			PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			this.appView.getSettings().setUserAgentString("iSCPediaMobile/" + pInfo.versionName + " " + currentUA);
+		} catch (NameNotFoundException e) {
+			// This never actually happens. Trust me, I'm an engineer!
+		}
+	}
+}
